65c56fd590cbbaad6b6613d973ac2e75ae5b8246
sg <sglee487@gmail.com>
2022-11-21 16:28:34 +0900

    commit base template

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <header>
+      <nav class="px-4 py-2.5 max-w-screen-xl">
+        <div class="py-2.5 flex flex-wrap justify-between items-center mx-auto">
+          <router-link to="/" class="p-1 cursor-pointer hover:bg-gray-400 rounded-md">
+            <div class="flex flex-row space-x-2">
+              <img src="./assets/line-chart.png" class="w-9 p-1 inline-block hover:bg-gray-400 rounded-md">
+              <h1 class="text-lg font-bold">Isolia</h1>
+            </div>
+          </router-link>
+          <div>
+            <ul class="flex flex-wrap justify-between items-center">
+              <li class="mr-4">
+                <router-link to="/">Home</router-link>
+              </li>
+            </ul>
+          </div>
+          <div class="space-x-2">
+            <a href="https://tistory-nari.tistory.com" target="_blank">
+              <img src="./assets/vue.svg" class="w-9 p-1 inline-block hover:bg-gray-400 rounded-md" />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+header {
+  font-family: 'Noto Sans KR', sans-serif;
+}
+</style>
