7c528e41db1613b5561b3ab6cb63fc1a0ecc49bd
sglee <sglee487@gmail.com>
2023-06-14 21:00:38 +0900

    feat: google login

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
 import { getCurrentInstance } from 'vue'
 
+import { useUserStore } from '@/stores/userStore'
+import { routerTo } from '@/utils/routerUtils'
+
 const instance = getCurrentInstance()
 const routerQuery = instance.proxy.$route.query
-console.log(instance.proxy.$route)
-console.log(routerQuery)
+const user = useUserStore()
 
 if (routerQuery && Object.keys(routerQuery).includes('userLoginResponse')) {
-  console.log(routerQuery.userLoginResponse)
-  console.log(JSON.parse(routerQuery.userLoginResponse))
+  const data = JSON.parse(routerQuery.userLoginResponse as string)
+  user.login(
+    data.jwt,
+    data.exp,
+    data.loginType,
+    data.email,
+    data.picture32,
+    data.picture96,
+    data.display_name,
+    data.role === 'admin'
+  )
+  routerTo(instance.proxy.$router, '/')
 }
 
 </script>
